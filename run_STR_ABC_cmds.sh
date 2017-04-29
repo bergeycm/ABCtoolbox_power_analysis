6ba4a651ce6ce844ae93316fae314f05a8fa251b
Christina Bergey <christina.bergey@gmail.com>
2017-04-29 14:48:45 -0400

    Added PBS scripts to run arlsumstat (serially or in parallel).
    This should ultimately be replaced by Makefile, etc.

---
+++ run_STR_ABC_cmds.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------------------------
+# --- Commands to run STR ABC power analysis
+# ----------------------------------------------------------------------------------------
+
+module load python/2.7.8
+
+# --- Create par files for DNA and STRs
+python scripts/fill_dna_par_file.py
+python scripts/fill_str_par_file.py
+
+# --- Simulate data under evolutionary scenario with parameter values defined in par files
+module load fastsimcoal/25221
+
+PAR_CT=`ls results/par_files/ | wc -l`
+qsub -t 1-$PAR_CT pbs/call_fastsimcoal.pbs
+
+# --- Call arlsumstat to compute summary stats
+module load arlsumstat/3522
+cp data/ssdefs.txt .
+cp data/arl_run.ars .
+
+ARP_DIR_CT=`find results/fastsimcoal_output/* -maxdepth 0 -type d | wc -l`
+qsub -t 1-$ARP_DIR_CT pbs/call_arlsumstat.pbs
+
+# For those that take awhile, you can instead use:
+# qsub -t 1-$ARP_DIR_CT pbs/call_arlsumstat_parallel.pbs
+
+rm ssdefs.txt
+rm arl_run.ars
